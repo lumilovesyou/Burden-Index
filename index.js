d03a0319e7a333511ef5669b2d5b389c25215081
mewo <felis.araneae@gmail.com>
2026-06-04 18:33:28 -0500

    Adding new items

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ let checkboxes = document.getElementsByClassName("checkbox");
 for (let i = 0; i < checkboxes.length; i++) {
 	checkboxes[i].addEventListener("click", () => {
 		//This is like really ugly but just putting !Boolean() around it doesn't work...
-		checkboxes[i].dataset.state = checkboxes[i].dataset.state == "true" ? false : true;
+		swapState(checkboxes[i]);
 	});
 };
+
+const addCheckboxButton = document.getElementById("addCheckbox");
+const addCheckboxText = document.getElementById("addCheckboxText");
+const taskList = document.getElementById("taskList");
+
+addCheckboxButton.addEventListener("click", () => {
+	let taskItem = document.createElement("li");
+	taskItem.innerHTML = `<span class="checkbox"></span> ${addCheckboxText.value}`
+	addCheckboxText.value = "";
+	taskList.insertBefore(taskItem, taskList.childNodes[taskList.childNodes.length - 2]);
+});
+
+function swapState(obj) {
+	if (!obj.classList.contains("disabled")) {
+		obj.dataset.state = obj.dataset.state == "true" ? false : true;
+	};
+}
